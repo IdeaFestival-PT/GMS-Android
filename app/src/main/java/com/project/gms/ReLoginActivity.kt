0b03a0f08c9b2ab6d376f9b7d1963a6b1331e709
hyeonseo0412 <s22026@gsm.hs.kr>
2022-12-25 19:25:42 +0900

    검색 layout 이미지 제외 구현

--- app/src/main/java/com/project/gms/ReLoginActivity.kt
+++ app/src/main/java/com/project/gms/ReLoginActivity.kt
@@ -1,11 +1,21 @@
 package com.project.gms
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.gms.databinding.ActivityLoginBinding
+import com.project.gms.databinding.ActivityReLoginBinding
 
 class ReLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityReLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_re_login)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/project/gms/SearchActivity.kt
@@ -0,0 +1,16 @@
+package com.project.gms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.gms.databinding.ActivitySearchBinding
+
+class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.myInformationBtn.bringToFront()
+    }
+}
